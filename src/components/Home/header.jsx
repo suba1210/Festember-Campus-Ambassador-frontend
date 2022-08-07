a571a22439229e3ab7b6886d1674e57c31b36a49
suba1210 <subasris1210@gmail.com>
2022-08-07 17:49:22 +0530

    feat: switch to typescript

--- src/components/Home/header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Header() {
-  return (
-    <h1>Festember Crew</h1>
-  );
-}
--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Header } from '../components/Home/header';
-
-export function Home() {
-  return (
-    <Header />
-  );
-}
